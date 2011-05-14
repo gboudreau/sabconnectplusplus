1eafa84c65e18cdcec919664e22ccde67002b949
gboudreau <github@danslereseau.com>
2011-05-14 18:02:58 +0000

    Issue 54: fixing 1-click for nzbclub.com

--- content_scripts/nzbclub.js
+++ content_scripts/nzbclub.js
@@ -1,9 +1,9 @@
 function addToSABnzbdFromNZBCLUB() {
     // Set the image to an in-progress image
     var img = chrome.extension.getURL('images/sab2_16_fetching.png');
-    $(this).attr("src", img);
+    $(this).find('img').first().attr("src", img);
     
-    var nzburl = 'http://nzbclub.com' + $(this).parent().attr('href');
+    var nzburl = 'http://nzbclub.com' + $(this).attr('href');
     var addLink = $(this).parent();
     	
     addToSABnzbd(addLink, nzburl, "addurl");
@@ -12,21 +12,13 @@ function addToSABnzbdFromNZBCLUB() {
 }
 
 function handleAllDownloadLinks() {
-	var unbind = false;
-	$('img[title="Download NZB"]').each(function() {
-		// Change the title to "Send to SABnzbd"
-		$(this).attr("title", "Send to SABnzbd");
-		
-		// Change the nzb download image
-		var img = chrome.extension.getURL('images/sab2_16.png');
-		$(this).attr("src", img);
-
-		// Change the on click handler to send to sabnzbd
-		$(this).click(addToSABnzbdFromNZBCLUB);		
-		unbind = true;
+	$('span[id^="ctl00_ContentPlaceHolder2_RadGrid1_ctl00_ctl"][id$="_ui_sizelabel"]').each(function() {
+	    var img = chrome.extension.getURL('/images/sab2_16.png');
+	    var href = $(this).find('a').attr('href');
+	    var link = '<span><a class="addSABnzbd" href="' + href + '"><img title="Send to SABnzbd" src="' + img + '" /> To SAB</a><br/></span>';
+	    $(this).after(link);
+		$(this).parent().find('a[class="addSABnzbd"]').first().click(addToSABnzbdFromNZBCLUB);
 	});
-	if ( unbind )
-		$("#ctl00_ContentPlaceHolder1_ui_searchformMain_ui_updatepanelMain").unbind("DOMNodeInserted", handleAllDownloadLinks);
 	return;
 }
 
@@ -34,5 +26,5 @@ chrome.extension.sendRequest({'action' : 'getContext'},function(response){
 	if (response.value.config.enable_nzbclub == "0") {
 		return;
 	}
-	$("#ctl00_ContentPlaceHolder1_ui_searchformMain_ui_updatepanelMain").bind("DOMNodeInserted", handleAllDownloadLinks);
+	handleAllDownloadLinks();
 });
