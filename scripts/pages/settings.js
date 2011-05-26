97b62c5f1ec89e2febdad4b032bb87bbdf0a5eed
rcdailey <rcdailey@gmail.com>
2011-05-26 14:00:06 +0000

    Some work on refreshing control states (doesn't work atm)

--- scripts/pages/settings.js
+++ scripts/pages/settings.js
@@ -1,13 +1,21 @@
-function OnResetConfigClicked()
+function RefreshControlStates( settings )
+{
+	var store = new Store( 'settings' );
+	Object.each( settings.manifest, function( setting ) {
+		setting.set( store.get( setting.params.name ) );
+	});
+}
+
+function OnResetConfigClicked( settings )
 {
 	var background = chrome.extension.getBackgroundPage();
 	background.resetSettings();
-	alert( 'Settings Reset' );
+	RefreshControlStates( settings );
 }
 
 function SetupEventHandlers( settings )
 {
-	settings.manifest.config_reset.addEvent( 'action', OnResetConfigClicked );
+	settings.manifest.config_reset.addEvent( 'action', bind( OnResetConfigClicked, settings ) );
 }
 
 window.addEvent("domready", function () {
