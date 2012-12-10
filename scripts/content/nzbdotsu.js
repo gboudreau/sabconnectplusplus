85d89408d3920fff79e37b1d85295131f33737aa
Benni-chan <benjamin@mycontact.eu>
2012-12-10 02:39:45 +0100

    added functions to add cart to sabnzbd

--- scripts/content/nzbdotsu.js
+++ scripts/content/nzbdotsu.js
@@ -5,7 +5,47 @@
 		oneClickImgTag = '<img style="vertical-align:top;" src="' + chrome.extension.getURL('/images/sab2_16.png') + '" />';
 
 	var baseUrl = 'https://'+window.location.host;
-			
+	
+	function addCart(e) {
+	
+		var $button = $(this);
+	
+		$button.val("Sending...").css('color', 'orange');	
+		console.log(e.data.selector)
+		$(e.data.selector).each(function() {
+			addOneCart($(this).closest('tr'));
+		});
+		
+		$button.val('Sent to SABnzbd!').css('color', '#0F0');
+		
+		setTimeout(function() {
+			$button.val('Send to SABnzbd').css('color', '#FFF');
+		}, 4000);
+		
+		return false;
+	}
+	
+	// $tr is a table row (one nzb)
+	function addOneCart($tr) { 
+	
+		var $anchor = $tr.find('a[title="View details"]');
+		var href = $anchor.attr('href').replace("details","getnzb");
+		var category = null;
+		if ($.trim($tr.parent().find('tr:nth-child(1)').find('th:nth-child(2)').text().toLowerCase()) == 'category') {
+		    category = $.trim($tr.find('td:nth-child(2) a').text().match(/^\s*([^> -]+)/)[1]);
+		} else if ($.trim($tr.parent().find('tr:nth-child(1)').find('th:nth-child(3)').text().toLowerCase()) == 'category') {
+		    category = $.trim($tr.find('td:nth-child(3) a').text().match(/^\s*([^> -]+)/)[1]);
+		}
+		
+		addToSABnzbd(
+			$anchor.get(0),
+			baseUrl + href + queryString,
+			'addurl',
+			null, 
+			category
+		);
+	}
+	
 	function addMany(e) {
 	
 		var $button = $(this);
@@ -25,8 +65,7 @@
 		return false;
 	}
 	
-	// $tr is a table row from #browsetable (one nzb)
-	// http://nzbs.org/getnzb/abef39dde2baaad865adecb95e5eb26d
+	// $tr is a table row (one nzb)
 	function addOne($tr) { 
 	
 		var $anchor = $tr.find('a.addSABnzbd');
@@ -76,12 +115,12 @@
 		;
 		
 		// Cart page: add a button above the list to send all NZBs to SAB
-		if ($('#main h1').text() === 'My Cart') {
+		if ($('div.span12 h1').text() === 'My Cart') {
 		
 			$('.nzb_multi_operations')
-				.prepend('<input type="button" value="Send Cart to SABnzbd" class="cartDownload" />')
+				.prepend('<input type="button" value="Send Cart to SABnzbd" class="btn btn-mini btn-primary cartDownload" />')
 				.find('input.cartDownload')
-				.on('click', {selector: 'tr:gt(0)'}, addMany)
+				.on('click', {selector: 'tr:gt(0)'}, addCart)
 			;
 		}
 	});
