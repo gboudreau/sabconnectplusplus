31badb0afe4b9d56ca69c6608f15a0b006887cc0
jsterken <jason@jsterken.com>
2013-01-19 12:19:55 -0600

    Revert "namespace sabconnect settings"
    This reverts commit 09d13980b43d1fcfab86e0cc55dc667b5632a6a7.

--- scripts/pages/settings.js
+++ scripts/pages/settings.js
@@ -1,4 +1,4 @@
-var store = new StoreClass('sabsettings', undefined, undefined, storeReady_settings);
+var store = new StoreClass('settings', undefined, undefined, storeReady_settings);
 
 function storeReady_settings() {
 	new FancySettings.initWithManifest( InitializeSettings );
